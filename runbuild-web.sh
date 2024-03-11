9702c3f716bc6e16b7d646eac84a232b1e709213
Feras <ferasboulala@gmail.com>
2024-03-11 18:20:04 -0400

    Small infra refactor

--- runbuild-web.sh
+++ runbuild-web.sh
@@ -3,5 +3,5 @@ set -e
 
 source ../emsdk/emsdk_env.sh
 
-emcmake cmake -B build -DUSE_EMSCRIPTEN=1 -DPLATFORM=Web
-cmake --build build -j$(sysctl -n hw.ncpu) --verbose
+emcmake cmake -B build-web -DUSE_EMSCRIPTEN=1 -DPLATFORM=Web
+cmake --build build-web -j$(sysctl -n hw.ncpu) --verbose
