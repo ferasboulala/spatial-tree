52883d621edd8c248cb48c7b319338f739e7a424
Feras <ferasboulala@gmail.com>
2025-02-03 17:08:15 -0500

    Optimize demo

--- examples/demo.cpp
+++ examples/demo.cpp
@@ -10,7 +10,7 @@ using CoordinateType = float;
 
 class my_spatial_set : public st::internal::spatial_tree<CoordinateType, Color, 2, 1, 32> {
 public:
-    void draw() const {
+    void draw(Image& img) const {
         const std::function<void(uint64_t, const st::bounding_box<CoordinateType, 2>&)>
             walk_recursively = [&](auto branch_index, auto boundary) {
                 assert(branch_index < this->branches_.size());
@@ -21,8 +21,8 @@ public:
                 }
 
                 auto [x, y] = boundary.origin();
-                DrawLine(x, boundary.starts[1], x, boundary.stops[1], GRAY);
-                DrawLine(boundary.starts[0], y, boundary.stops[0], y, GRAY);
+                ImageDrawLine(&img, x, boundary.starts[1], x, boundary.stops[1], GRAY);
+                ImageDrawLine(&img, boundary.starts[0], y, boundary.stops[0], y, GRAY);
 
                 st::internal::unroll_for<BranchingFactor>([&](auto child) {
                     const uint64_t child_index = branch.index_of_first_child + child;
@@ -71,12 +71,14 @@ int main() {
 
     my_spatial_set quadtree({0, 0, WindowWidth, WindowHeight});
 
-    std::default_random_engine       gen;
-    std::normal_distribution<double> normal(0.0, 1.0);
-    uint64_t                         brush_size = 20;
-    int64_t                          brush_radius = 20;
-    double                           color_counter = 0;
+    std::vector<std::array<CoordinateType, 2> > points_to_erase;
+    std::default_random_engine                  gen;
+    std::normal_distribution<double>            normal(0.0, 1.0);
+    uint64_t                                    brush_size = 20;
+    int64_t                                     brush_radius = 20;
+    double                                      color_counter = 0;
 
+    SetTraceLogLevel(TraceLogLevel::LOG_FATAL);
     InitWindow(WindowWidth, WindowHeight, "draw");
     SetTargetFPS(60);
 
@@ -88,16 +90,13 @@ int main() {
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        quadtree.draw();
-
-        DrawFPS(10, 10);
-        const std::string size_str = std::to_string(quadtree.size());
-        DrawText(size_str.c_str(), 10, 30, 10, DARKGRAY);
-        DrawText("LMB to draw, RMB to erase, wheel to increase brush size", 10, 40, 10, DARKGRAY);
+        Image img = GenImageColor(GetScreenWidth(), GetScreenHeight(), WHITE);
+        for (auto [pos, color] : quadtree) {
+            ImageDrawPixel(&img, pos[0], pos[1], color);
+        }
 
         brush_size = std::max<uint64_t>(1, M_PI * brush_radius * brush_radius / 1000.0);
         auto mouse_position = GetMousePosition();
-
         if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
             for (uint64_t i = 0; i < brush_size; ++i) {
                 auto offset_x = normal(gen) * brush_radius / 2;
@@ -110,13 +109,20 @@ int main() {
                 }
             }
             color_counter += GetFrameTime();
-        } else if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
-            std::vector<std::array<CoordinateType, 2> > points_to_erase;
-            quadtree.find(st::sphere<CoordinateType, 2>{static_cast<CoordinateType>(brush_radius),
-                                                        {mouse_position.x, mouse_position.y}},
-                          [&](auto it) { points_to_erase.push_back((*it).first); });
+        }
+
+        points_to_erase.clear();
+        quadtree.find(st::sphere<CoordinateType, 2>{static_cast<CoordinateType>(brush_radius),
+                                                    {mouse_position.x, mouse_position.y}},
+                      [&](auto it) { points_to_erase.push_back((*it).first); });
+
+        if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
             for (auto point : points_to_erase) {
                 quadtree.erase(point);
+            }
+        } else {
+            for (auto point : points_to_erase) {
+                ImageDrawPixel(&img, point[0], point[1], RED);
             }
         }
 
@@ -126,13 +132,22 @@ int main() {
             brush_radius = std::max<int64_t>(1, brush_radius);
         }
 
+        if (IsKeyDown(KEY_D)) {
+            quadtree.draw(img);
+        }
+
+        Texture tex = LoadTextureFromImage(img);
+        UnloadImage(img);
+        DrawTexture(tex, 0, 0, WHITE);
+
+        DrawFPS(10, 10);
+        const std::string size_str = std::to_string(quadtree.size());
+        DrawText(size_str.c_str(), 10, 30, 10, DARKGRAY);
+        DrawText("LMB to draw, RMB to erase, wheel to increase brush size", 10, 40, 10, DARKGRAY);
+
         if (!quadtree.empty()) {
             auto [nearest, _] = *quadtree.nearest({mouse_position.x, mouse_position.y}, 1).front();
             DrawLine(mouse_position.x, mouse_position.y, nearest[0], nearest[1], GRAY);
-        }
-
-        for (auto [pos, color] : quadtree) {
-            DrawCircle(pos[0], pos[1], 2, color);
         }
 
         DrawCircleLines(mouse_position.x, mouse_position.y, brush_radius, GRAY);
